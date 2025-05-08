ee7062fa79e7b12d3bc15f896d53865ba8e0c317
Sajmul <sajmul1427@gmail.com>
2025-05-08 21:58:41 +0600

    first two task done

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+const formatString = (input: string, toUpper?: boolean): string => {
+    if(toUpper === false) {
+        return input.toLowerCase();
+    }
+
+    return input.toUpperCase();
+}
+
+
+const filterByRating = (
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] => {
+    const filteredArray = items.filter((item: {title: string, rating: number}) => item?.rating >= 4);
+    return filteredArray;
+}
+
+const concatenateArrays<T> = (...arrays: T[][]): T[] => {
+    return []
+}
