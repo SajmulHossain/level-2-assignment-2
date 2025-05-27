032b69aba815c0b6d6dfc58c827300a09e3071e5
Sajmul <sajmul1427@gmail.com>
2025-05-27 22:10:27 +0600

    task 5 done

--- assignment_2.sql
+++ assignment_2.sql
@@ -4,9 +4,6 @@ CREATE TABLE rangers (
     region VARCHAR(50) NOT NULL
 );
 
-SELECT * FROM rangers;
-SELECT * FROM species;
-SELECT * FROM sightings;
 CREATE TABLE species(
     species_id SERIAL PRIMARY KEY,
     common_name VARCHAR(50) NOT NULL,
@@ -18,13 +15,16 @@ CREATE TABLE species(
 CREATE TABLE sightings (
     sighting_id SERIAL PRIMARY KEY,
     ranger_id INT REFERENCES rangers(ranger_id),
-    spacies_id INT REFERENCES species(species_id),
+    species_id INT REFERENCES species(species_id),
     sighting_time TIMESTAMP,
     location VARCHAR(50),
     notes TEXT
 );
 
+
 INSERT INTO rangers(name, region) VALUES('Alice Green', 'Nothern Hills'), ('Boob White', 'River Delta'), ('Carol King', 'Mountain Range');
+
+SELECT * FROM rangers;
 
 INSERT INTO species(common_name, scientific_name, discovery_date, conservation_status)
  VALUES 
@@ -35,7 +35,7 @@ INSERT INTO species(common_name, scientific_name, discovery_date, conservation_s
 
  SELECT * FROM species;
 
- INSERT INTO sightings(spacies_id, ranger_id, location, sighting_time, notes)
+ INSERT INTO sightings(species_id, ranger_id, location, sighting_time, notes)
  VALUES
  (1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
  (2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
@@ -49,13 +49,20 @@ INSERT INTO rangers(name, region) VALUES
 ('Derek Fox', 'Coastal Plains');
 
 -- * task -- 2
-SELECT COUNT(DISTINCT spacies_id) as unique_species_count FROM sightings;
+SELECT COUNT(DISTINCT species_id) as unique_species_count FROM sightings;
 
 -- * task -- 3
 SELECT * FROM sightings WHERE location ILIKE '%Pass%';
 
 -- * task -- 4
 SELECT name, count(sighting_id) AS total_sightings FROM rangers JOIN sightings USING(ranger_id) GROUP BY name;
+
+-- * task -- 5
+SELECT common_name FROM species 
+    LEFT JOIN sightings ON species.species_id = sightings.species_id
+    WHERE sightings.species_id IS NULL;
+
+    SELECT * FROM sightings;
 
 -- * task -- 6
 SELECT common_name, sighting_time, name FROM sightings 
